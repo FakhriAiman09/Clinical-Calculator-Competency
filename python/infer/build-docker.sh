fc5c479d0d4bb796b0a4c8dd25f0fe079e82faca
atlantisel <80100913+atlantisel@users.noreply.github.com>
2025-04-16 16:33:17 -0400

    fix: comment out Docker BuildKit and secret options in build script

--- python/infer/build-docker.sh
+++ python/infer/build-docker.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-DOCKER_BUILDKIT=1 docker build \
+# DOCKER_BUILDKIT=1
+docker build \
   --platform linux/amd64 \
-  --secret id=kaggle,src=kaggle.json \
-  --secret id=env,src=.env \
   -t cccalculator/infer-server .
+
+# --secret id=kaggle,src=kaggle.json \
+# --secret id=env,src=.env \
