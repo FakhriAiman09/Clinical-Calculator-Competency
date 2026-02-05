ac94321cd8e184c883ba4efa18fa79e3652f88b8
nrrfatihahh <nva5479@psu.edu>
2026-02-05 14:32:28 -0500

    Update send-email.server.ts

--- frontend/src/app/dashboard/student/form-requests/email-api/send-email.server.ts
+++ frontend/src/app/dashboard/student/form-requests/email-api/send-email.server.ts
@@ -21,12 +21,15 @@ export async function sendEmail({
     port: 465,
     secure: true,
     auth: {
-      user: process.env.SMTP_USER ,
+      user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS
     },
   });
 
   try {
+    console.log("Sending email to:", to);
+    console.log("Using SMTP user:", process.env.SMTP_USER);
+
     const info = await transporter.sendMail({
       from: '"Clinical Competency Calculator" <clinicalcompetencycalculator@gmail.com>',
       to, 
@@ -34,10 +37,25 @@ export async function sendEmail({
       text: `You have been requested to fill out a form by ${studentName}`,
     });
     return { message: 'Email sent successfully', id: info.messageId };
-  } catch (error: unknown) {
+  } 
+  /*
+  catch (error: unknown) {
     if (error instanceof Error) {
       throw new Error(error.message || 'Error sending email');
     }
     throw new Error('Error sending email');
+  }*/
+
+  catch (error: unknown) {
+  console.error("EMAIL ERROR FULL:", error);
+
+  if (error instanceof Error) {
+    console.error("EMAIL ERROR MESSAGE:", error.message);
+    console.error("EMAIL ERROR STACK:", error.stack);
+    throw new Error(`Email failed: ${error.message}`);
   }
+
+  throw new Error('Email failed: unknown error');
+}
+
 }
