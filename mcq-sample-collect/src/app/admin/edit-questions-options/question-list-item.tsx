4126f167437c2cdf99dc48cb0d95dd388bd0cef9
nrrfatihahh <nva5479@psu.edu>
2026-04-15 16:49:03 -0400

    fix duplicate

--- mcq-sample-collect/src/app/admin/edit-questions-options/question-list-item.tsx
+++ mcq-sample-collect/src/app/admin/edit-questions-options/question-list-item.tsx
@@ -3,53 +3,67 @@ import type { MCQ } from '@/utils/types';
 import OptionItem from './question-list-option-item';
 import { renderQuestion } from './render-spans';
 
+type QuestionItemProps = Readonly<{
+  i: number;
+  mcq: MCQ;
+  handleOptionClick: (mcq: MCQ, key: string, text: string) => void;
+  handleQuestionClick: (mcq: MCQ) => void;
+}>;
+
 export default function QuestionItem({
   i,
   mcq,
   handleOptionClick,
   handleQuestionClick,
-}: {
-  i: number;
-  mcq: MCQ;
-  handleOptionClick: (mcq: MCQ, key: string, text: string) => void;
-  handleQuestionClick: (mcq: MCQ) => void;
-}) {
+}: QuestionItemProps) {
+  const headingId = `heading-${i}`;
+  const collapseId = `collapse-${i}`;
+  const questionToggleProps = {
+    className: 'accordion-button collapsed gap-1',
+    type: 'button' as const,
+    'data-bs-toggle': 'collapse',
+    'data-bs-target': `#${collapseId}`,
+    'aria-expanded': 'false' as const,
+    'aria-controls': collapseId,
+  };
+  const editQuestionModalProps = {
+    'data-bs-toggle': 'modal',
+    'data-bs-target': '#edit-question-modal',
+  };
+  const optionRows = Object.entries(mcq.options).map(([optionKey, optionValue]) => (
+    <OptionItem
+      key={optionKey}
+      optKey={optionKey}
+      value={optionValue}
+      mcq={mcq}
+      handleOptionClick={handleOptionClick}
+    />
+  ));
+
   return (
     <div className='accordion-item'>
-      <h4 className='accordion-header' id={`heading-${i}`}>
-        <button
-          className='accordion-button collapsed gap-1'
-          type='button'
-          data-bs-toggle='collapse'
-          data-bs-target={`#collapse-${i}`}
-          aria-expanded='false'
-          aria-controls={`collapse-${i}`}
-        >
+      <h4 className='accordion-header' id={headingId}>
+        <button {...questionToggleProps}>
           {renderQuestion(mcq.kf, mcq.question)}
         </button>
       </h4>
       <div
-        id={`collapse-${i}`}
+        id={collapseId}
         className='accordion-collapse collapse bg-body-secondary'
-        aria-labelledby={`heading-${i}`}
+        aria-labelledby={headingId}
         data-bs-parent='#question-list'
       >
         <div className='d-flex justify-content-end'>
           <button
             className='btn btn-link m-2 mb-0'
             onClick={() => handleQuestionClick(mcq)}
-            data-bs-toggle='modal'
-            data-bs-target='#edit-question-modal'
+            {...editQuestionModalProps}
           >
             <span className='me-2'>Edit question</span>
             <i className='bi bi-pencil'></i>
           </button>
         </div>
-        <ul className='list-group p-2'>
-          {Object.entries(mcq.options).map(([key, value]) => (
-            <OptionItem key={key} optKey={key} value={value} mcq={mcq} handleOptionClick={handleOptionClick} />
-          ))}
-        </ul>
+        <ul className='list-group p-2'>{optionRows}</ul>
       </div>
     </div>
   );
